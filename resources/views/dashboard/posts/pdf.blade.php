b0e495d6c646e1c4cd8bf53f2dc450874cee9fc7
wedisDev <wedisdotco@gmail.com>
2022-07-06 10:08:02 +0700

    pdf title

--- resources/views/dashboard/posts/pdf.blade.php
+++ resources/views/dashboard/posts/pdf.blade.php
@@ -14,7 +14,7 @@
 
 <body>
 
-    <h1 align="center">Laporan History Appoiments</h1>
+    <h1 align="center">Laporan History Appointmegnts</h1>
 
     <div class="table-responsive">
         <table class="table" id="tableKategori">
